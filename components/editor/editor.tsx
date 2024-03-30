80b093b3204ae78b542bfc54818adafd857ab5ba
Kian Bazarjani <kian@bazza.dev>
2024-03-30 14:15:05 -0400

    Add vercel-build command to migrate db, remove commands from editor

--- components/editor/editor.tsx
+++ components/editor/editor.tsx
@@ -5,19 +5,22 @@ import "@/components/editor/prosemirror.css";
 import {
   EditorContent,
   EditorRoot,
-  EditorCommand,
-  EditorCommandItem,
-  EditorCommandEmpty,
-  EditorCommandList,
+  // EditorCommand,
+  // EditorCommandItem,
+  // EditorCommandEmpty,
+  // EditorCommandList,
 } from "novel";
 import { defaultExtensions } from "./extensions";
-import { suggestionItems, slashCommand } from "./slash-menu";
+// import { suggestionItems, slashCommand } from "./slash-menu";
 import { handleCommandNavigation } from "novel/extensions";
 import { useAtom } from "jotai";
 import { editorAtom } from "@/lib/atoms";
 import { cn } from "@/lib/utils";
 
-const extensions = [...defaultExtensions, slashCommand];
+const extensions = [
+  ...defaultExtensions,
+  // slashCommand
+];
 
 interface Props {
   updateSessionNotes?: (notes: string) => void;
@@ -43,10 +46,10 @@ const Editor = ({
             !viewOnly && "p-12 px-8 sm:px-12 border shadow-lg",
           )}
           onUpdate={({ editor }) => {
-            setEditor(editor);
+            setEditor(editor as any);
             const json = editor.getJSON();
             const str = JSON.stringify(json);
-            updateSessionNotes(str);
+            updateSessionNotes?.(str);
           }}
           initialContent={initialContentJSON}
           editable={!viewOnly}
@@ -60,33 +63,34 @@ const Editor = ({
             },
           }}
         >
-          <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
-            <EditorCommandEmpty className="px-2 text-sm text-muted-foreground">
-              No results.
-            </EditorCommandEmpty>
-            <EditorCommandList>
-              {suggestionItems.map((item) => (
-                <EditorCommandItem
-                  value={item.title}
-                  onCommand={(val) =>
-                    item.command ? item.command(val) : false
-                  }
-                  className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent `}
-                  key={item.title}
-                >
-                  <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
-                    {item.icon}
-                  </div>
-                  <div>
-                    <p className="font-medium">{item.title}</p>
-                    <p className="text-xs text-muted-foreground">
-                      {item.description}
-                    </p>
-                  </div>
-                </EditorCommandItem>
-              ))}
-            </EditorCommandList>
-          </EditorCommand>
+          <div></div>
+          {/* <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all"> */}
+          {/*   <EditorCommandEmpty className="px-2 text-sm text-muted-foreground"> */}
+          {/*     No results. */}
+          {/*   </EditorCommandEmpty> */}
+          {/*   <EditorCommandList> */}
+          {/*     {suggestionItems.map((item) => ( */}
+          {/*       <EditorCommandItem */}
+          {/*         value={item.title} */}
+          {/*         onCommand={(val) => */}
+          {/*           item.command ? item.command(val) : false */}
+          {/*         } */}
+          {/*         className={`flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:bg-accent aria-selected:bg-accent `} */}
+          {/*         key={item.title} */}
+          {/*       > */}
+          {/*         <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background"> */}
+          {/*           {item.icon} */}
+          {/*         </div> */}
+          {/*         <div> */}
+          {/*           <p className="font-medium">{item.title}</p> */}
+          {/*           <p className="text-xs text-muted-foreground"> */}
+          {/*             {item.description} */}
+          {/*           </p> */}
+          {/*         </div> */}
+          {/*       </EditorCommandItem> */}
+          {/*     ))} */}
+          {/*   </EditorCommandList> */}
+          {/* </EditorCommand> */}
         </EditorContent>
       </EditorRoot>
     </div>
