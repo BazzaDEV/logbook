faf0ccf476e033d1c5b4a143677e006d5adf1d85
Kian Bazarjani <kian@bazza.dev>
2024-03-31 15:43:26 -0400

    Clean slate for v2

--- app/fonts.ts
+++
@@ -1,4 +0,0 @@
-import { Inter, JetBrains_Mono } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"] });
-export const jetbrainMono = JetBrains_Mono({ subsets: ["latin"] });
--- prisma/migrations/20240329192538_delete_events_on_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "WorkSessionEvent" DROP CONSTRAINT "WorkSessionEvent_sessionId_fkey";
-
--- AddForeignKey
-ALTER TABLE "WorkSessionEvent" ADD CONSTRAINT "WorkSessionEvent_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "WorkSession"("id") ON DELETE CASCADE ON UPDATE CASCADE;
