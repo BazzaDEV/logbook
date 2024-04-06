5455da035bde09ef90a57506da29a54e8bebee7e
Kian Bazarjani <kian@bazza.dev>
2024-04-06 13:03:27 -0400

    Add icon to coming soon page for sessions

--- src/app/(app)/sessions/page.tsx
+++ src/app/(app)/sessions/page.tsx
@@ -1,9 +1,12 @@
+import { GalleryVerticalEnd } from 'lucide-react'
+
 export default function Page() {
   return (
     <div className="flex-1 flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-bold tracking-tighter text-zinc-100">
-        Coming soon...
-      </h1>
+      <div className="flex flex-col items-center gap-10 text-zinc-100">
+        <GalleryVerticalEnd className="size-24" />
+        <h1 className="text-6xl font-bold tracking-tighter">Coming soon...</h1>
+      </div>
     </div>
   )
 }
